971cd1a97d6895b768275fc6735ff032b50802d2
Gui-Parreira <gui.parreira98@gmail.com>
2024-04-08 20:36:57 -0300

    Ajuste de buscas lançamentos, end point de todos episodios e episodios por temporada

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -30,7 +30,7 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie AND YEAR(e.dataLancamento) >= :anoLancamento")
     List<Episodio> episodiosPorSerieEAno(Serie serie, int anoLancamento);
 
-    List<Serie> findTop5ByOrderByEpisodiosDataLancamentoDesc();
+    //List<Serie> findTop5ByOrderByEpisodiosDataLancamentoDesc();
 
     @Query("SELECT s FROM Serie s " +
             "JOIN s.episodios e " +
@@ -38,4 +38,6 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
             "ORDER BY MAX(e.dataLancamento) DESC LIMIT 5")
     List<Serie> encontrarEpisodiosMaisRecentes();
 
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numero")
+    List<Episodio> obterEpisodiosPorTemporada(Long id, Long numero);
 }
